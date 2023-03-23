c664d94f80b572d5ca0521cd4bc37e224cd9bff3
Jason Leech <jason.leech@digipen.edu>
2023-03-23 14:25:30 -0700

    Add player animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,16 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D myRB2D;
     public float moveSpeed;
+
+    private Animator _animator;
     
     // Start is called before the first frame update
     void Start()
     {
         myRB2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        myRB2D.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+        myRB2D.velocity = velocity;
+        if (velocity.normalized.sqrMagnitude > 0.1)
+        {
+            _animator.SetBool("walking", true);
+            _animator.SetFloat("X", velocity.normalized.x);
+            _animator.SetFloat("Y", velocity.normalized.y);
+        }
+        else
+        {
+            _animator.SetBool("walking", false);
+        }
+
     }
 }
