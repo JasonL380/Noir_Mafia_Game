5abac49a7f2ec5b9ce2b1620650f68dd859736b4
danieldavidai <danieldavidai5@gmail.com>
2023-03-29 13:41:12 -0700

    PlayButton.cs

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,17 @@
+// Name: Jason Leech
+// Date: 03/29/2023
+// Desc:
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class PlayButton : MonoBehaviour
+    {
+        public void play()
+        {
+            SceneManager.LoadScene("PlayScene");
+        }
+    }
+}
