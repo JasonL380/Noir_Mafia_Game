533a2b4d7942c07f0e04fbdf977abf9fad7adc5e
Jason Leech <jason.leech@digipen.edu>
2023-03-29 14:24:52 -0700

    Fix build errors

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using DefaultNamespace;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
